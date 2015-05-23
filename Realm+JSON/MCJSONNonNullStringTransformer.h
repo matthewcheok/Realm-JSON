435671555b47247dbaed41abc60c54b897124458
Matthew Cheok <cheok.jz@gmail.com>
2015-05-23 09:42:07 -0700

    Updated to include non null string transformer

---
+++ Realm+JSON/MCJSONNonNullStringTransformer.h
@@ -0,0 +1,15 @@
+//
+//  MCJSONNonNullStringTransformer.h
+//  Pods
+//
+//  Created by Matthew Cheok on 23/5/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCJSONNonNullStringTransformer : NSValueTransformer
+
++ (instancetype)valueTransformer;
+
+@end
